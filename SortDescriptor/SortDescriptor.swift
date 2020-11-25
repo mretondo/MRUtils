a6163c377cfc074c83fbb80678f5ca6ce6489c54
Mike Retondo <mretondo@mac.com>
2020-11-24 21:45:22 -0600

    Initial Commit

---
+++ SortDescriptor/SortDescriptor.swift
@@ -0,0 +1,11 @@
+//
+//  SortDescriptor.swift
+//  SortDescriptor
+//
+//  Created by Mike Retondo on 11/24/20.
+//  Copyright © 2020 Mike Retondo. All rights reserved.
+//
+
+class SortDescriptor {
+
+}
